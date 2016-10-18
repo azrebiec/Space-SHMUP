9cbaec6a187c723585de3d58f7db0a65b2adb504
azrebiec <andrew.zrebiec@rutgers.edu>
2016-10-18 18:29:23 -0400

    created main script

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -10,6 +10,10 @@ public class Enemy : MonoBehaviour {
 	public Bounds bounds; // The Bounds of this and its children
 	public Vector3 boundsCenterOffset; // Dist of bounds.center from position
 
+	void Awake() {
+		InvokeRepeating( "CheckOffscreen", 0f, 2f );
+	}
+
 	// Use this for initialization
 	void Start () {
 	
@@ -32,6 +36,27 @@ public class Enemy : MonoBehaviour {
 		}
 		set {
 			this.transform.position = value;
+		}
+	}
+
+	void CheckOffscreen() {
+		// If bounds are still their default value...
+		if (bounds.size == Vector3.zero) {
+			// then set them
+			bounds = Utils.CombineBoundsOfChildren(this.gameObject);
+			// Also find the diff between bounds.center & transform.position
+			boundsCenterOffset = bounds.center - transform.position;
+		}
+		// Every time, update the bounds to the current position
+		bounds.center = transform.position + boundsCenterOffset;
+		// Check to see whether the bounds are completely offscreen
+		Vector3 off = Utils.ScreenBoundsCheck( bounds, BoundsTest.offScreen );
+		if ( off != Vector3.zero ) {
+			// If this enemy has gone off the bottom edge of the screen
+			if (off.y < 0) {
+				// then destroy it
+				Destroy( this.gameObject );
+			}
 		}
 	}
 }
