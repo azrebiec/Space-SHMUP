46c0168795f4eb501af8e623d8e00a1d22e80a9c
azrebiec <andrew.zrebiec@rutgers.edu>
2016-10-18 18:22:06 -0400

    added bound code to utils and hero, created enemy prefabs

--- Assets/_Scripts/Utils.cs
+++ Assets/_Scripts/Utils.cs
@@ -2,6 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 
+//This is actually OUTSIDE of the Utils Class
+public enum BoundsTest{
+	center,		//is the center of the GameObject on screen?
+	onScreen,	//Are the bounds entirely on screen?
+	offScreen	//Are the bounds entirely off screen?
+}
+
 public class Utils : MonoBehaviour {
 	//============================= Bounds Functions =============================\\
 	// Creates bounds that encapsulate the two Bounds passed in.
@@ -24,14 +31,14 @@ public class Utils : MonoBehaviour {
 		// Create an empty Bounds b
 		Bounds b = new Bounds(Vector3.zero, Vector3.zero);
 		// If this GameObject has a Renderer Component...
-		if (go.renderer != null) {
+		if (go.GetComponent<Renderer>() != null) {
 			// Expand b to contain the Renderer's Bounds
-			b = BoundsUnion(b, go.renderer.bounds);
+			b = BoundsUnion(b, go.GetComponent<Renderer>().bounds);
 		}
 		// If this GameObject has a Collider Component...
-		if (go.collider != null) {
+		if (go.GetComponent<Collider>() != null) {
 			// Expand b to contain the Collider's Bounds
-			b = BoundsUnion(b, go.collider.bounds);
+			b = BoundsUnion(b, go.GetComponent<Collider>().bounds);
 		}
 		// Recursively iterate through each child of this gameObject.transform
 		foreach( Transform t in go.transform ) { // 1
@@ -40,6 +47,137 @@ public class Utils : MonoBehaviour {
 		}
 		return( b );
 	}
+
+	//Make a static read-only public property camBounds
+	static public Bounds camBounds {
+		get {
+			//if _camBounds hasn't been set yet
+			if (_camBounds.size == Vector3.zero) {
+				//SetCameraBounds using the default Camera
+				SetCameraBounds();
+			}
+			return (_camBounds);
+		}
+	}
+	//This is the private static field that camBounds uses
+	static private Bounds _camBounds;
+
+	public static void SetCameraBounds (Camera cam=null){
+		//if no camera was passed in, use the main camera
+		if (cam == null) cam = Camera.main;
+		//This makes a couple important assumptions about the camera!:
+		//1. the camera is orthographic
+		//2. the camera is at a rotation of R:[0,0,0]
+
+		//Make Vector3s at the topLeft and bottomRight of the Screen coords
+		Vector3 topLeft = new Vector3 (0, 0, 0);
+		Vector3 bottomRight = new Vector3 (Screen.width, Screen.height, 0);
+	
+		//Convert these to world coordinates
+		Vector3 boundTLN = cam.ScreenToWorldPoint (topLeft);
+		Vector3 boundBRF = cam.ScreenToWorldPoint (bottomRight);
+
+		//adjust the z to be at the near and far camera clipping planes
+		boundTLN.z += cam.nearClipPlane;
+		boundBRF.z += cam.farClipPlane;
+
+		//find the center of the bounds
+		Vector3 center = (boundTLN + boundBRF) / 2f;
+		_camBounds = new Bounds (center, Vector3.zero);
+		//expand _camBounds to encapsulate the extents.
+		_camBounds.Encapsulate (boundTLN);
+		_camBounds.Encapsulate (boundBRF);
+		
+	}
+
+	// Checks to see whether the Bounds bnd are within the camBounds
+	public static Vector3 ScreenBoundsCheck(Bounds bnd, BoundsTest
+		test = BoundsTest.center) {
+		return( BoundsInBoundsCheck( camBounds, bnd, test ) );
+	}
+	// Checks to see whether Bounds lilB are within Bounds bigB
+	public static Vector3 BoundsInBoundsCheck( Bounds bigB, Bounds lilB, BoundsTest
+		test = BoundsTest.onScreen ) {
+		// The behavior of this function is different based on the BoundsTest
+		// that has been selected.
+		// Get the center of lilB
+		Vector3 pos = lilB.center;
+		// Initialize the offset at [0,0,0]
+		Vector3 off = Vector3.zero;
+		switch (test) {
+		// The center test determines what off (offset) would have to be applied
+		// to lilB to move its center back inside bigB
+		case BoundsTest.center:
+			if ( bigB.Contains( pos ) ) {
+				return( Vector3.zero );
+			}
+			if (pos.x > bigB.max.x) {
+				off.x = pos.x - bigB.max.x;
+			} else if (pos.x < bigB.min.x) {
+				off.x = pos.x - bigB.min.x;
+			}
+			if (pos.y > bigB.max.y) {
+				off.y = pos.y - bigB.max.y;
+			} else if (pos.y < bigB.min.y) {
+				off.y = pos.y - bigB.min.y;
+			}
+			if (pos.z > bigB.max.z) {
+				off.z = pos.z - bigB.max.z;
+			} else if (pos.z < bigB.min.z) {
+				off.z = pos.z - bigB.min.z;
+			}
+			return( off );
+			// The onScreen test determines what off would have to be applied to
+			// keep all of lilB inside bigB
+		case BoundsTest.onScreen:
+			if ( bigB.Contains( lilB.min ) && bigB.Contains( lilB.max ) ) {
+				return( Vector3.zero );
+			}
+			if (lilB.max.x > bigB.max.x) {
+				off.x = lilB.max.x - bigB.max.x;
+			} else if (lilB.min.x < bigB.min.x) {
+				off.x = lilB.min.x - bigB.min.x;
+			}
+			if (lilB.max.y > bigB.max.y) {
+				off.y = lilB.max.y - bigB.max.y;
+			} else if (lilB.min.y < bigB.min.y) {
+				off.y = lilB.min.y - bigB.min.y;
+			}
+			if (lilB.max.z > bigB.max.z) {
+				off.z = lilB.max.z - bigB.max.z;
+			} else if (lilB.min.z < bigB.min.z) {
+				off.z = lilB.min.z - bigB.min.z;
+			}
+			return( off );
+			// The offScreen test determines what off would need to be applied to
+			// move any tiny part of lilB inside of bigB
+		case BoundsTest.offScreen:
+			bool cMin = bigB.Contains( lilB.min );
+			bool cMax = bigB.Contains( lilB.max );
+			if ( cMin || cMax ) {
+				return( Vector3.zero );
+			}
+			if (lilB.min.x > bigB.max.x) {
+				off.x = lilB.min.x - bigB.max.x;
+			} else if (lilB.max.x < bigB.min.x) {
+				off.x = lilB.max.x - bigB.min.x;
+			}
+			if (lilB.min.y > bigB.max.y) {
+				off.y = lilB.min.y - bigB.max.y;
+			} else if (lilB.max.y < bigB.min.y) {
+				off.y = lilB.max.y - bigB.min.y;
+			}
+			if (lilB.min.z > bigB.max.z) {
+				off.z = lilB.min.z - bigB.max.z;
+			} else if (lilB.max.z < bigB.min.z) {
+				off.z = lilB.max.z - bigB.min.z;
+			}
+			return( off );
+		}
+		return( Vector3.zero );
+	}
+
+
 
 	// Use this for initialization
 	void Start () {
