16a990d284fc1341b03dde138bd9b9a3c51beaaa
azrebiec <andrew.zrebiec@rutgers.edu>
2016-10-18 19:03:44 -0400

    created weapon prefab and script

--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class Hero : MonoBehaviour {
 	static public Hero S; // Singleton
+	public float gameRestartDelay = 2f;
 	// These fields control the movement of the ship
 	public float speed = 30;
 	public float rollMult = -45;
 	public float pitchMult = 30;
 	// Ship status information
-	public float shieldLevel = 1;
+	[SerializeField]
+	private float _shieldLevel = 1; // Add the underscore!
 	public bool ____________________________;
 	public Bounds bounds;
 	void Awake() {
@@ -45,7 +47,47 @@ public class Hero : MonoBehaviour {
 		transform.rotation = Quaternion.Euler(yAxis*pitchMult,xAxis*rollMult,0);
 	}
 
+	// This variable holds a reference to the last triggering GameObject
+	public GameObject lastTriggerGo = null; // 1
 	void OnTriggerEnter(Collider other) {
-		print("Triggered: "+other.gameObject.name);
+		// Find the tag of other.gameObject or its parent GameObjects
+		GameObject go = Utils.FindTaggedParent(other.gameObject);
+		// If there is a parent with a tag
+		if (go != null) {
+			// Announce it
+			// Make sure it's not the same triggering go as last time
+			if (go == lastTriggerGo) { // 2
+				return;
+			}
+			lastTriggerGo = go; // 3
+			if (go.tag == "Enemy") {
+				// If the shield was triggered by an enemy
+				// Decrease the level of the shield by 1
+				shieldLevel--;
+				// Destroy the enemy
+				Destroy(go); // 4
+			} else {
+				print("Triggered: "+go.name); // Move this line here!
+			}
+		} else {
+			// Otherwise announce the original other.gameObject
+			print("Triggered: "+other.gameObject.name); // Move this line here!
+		}
 	}
+
+	public float shieldLevel {
+		get {
+			return( _shieldLevel ); // 1
+		}
+		set {
+			_shieldLevel = Mathf.Min( value, 4 ); // 2
+			// If the shield is going to be set to less than zero
+			if (value < 0) { // 3
+				Destroy(this.gameObject);
+				// Tell Main.S to restart the game after a delay
+				Main.S.DelayedRestart( gameRestartDelay );
+			}
+		}
+	}
+
 }
